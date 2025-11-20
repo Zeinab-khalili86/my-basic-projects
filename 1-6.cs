a2422f2e33a18af30875a557465045ffff9178d9
Zeinab-khalili86 <zinatkhalili3@gmail.com>
2025-11-20 22:57:19 +0330

    Add files via upload
    The program above is for calculating the tax percentage, the insurance percentage, and deducting them from the received salary. These programs help train our logic so that in other situations—not only salary calculation—we can use coding to subtract or add certain percentages to an initial value and display a final amount as the output

---
+++ 1-6.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] moadel = new double[10];
+            int i;
+            double max, min;
+            Console.Write(" moadel[0]=");
+            moadel[0] = double.Parse(Console.ReadLine());
+            max = moadel[0]; min = moadel[0];
+            double sum = moadel[0];
+            for (i = 1; i < moadel.Length; i++)
+            {
+                Console.Write(" moadel[{0}]=", i);
+                moadel[i] = double.Parse(Console.ReadLine());
+                sum += moadel[i];
+            }
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            for (i = 0; i < moadel.Length; i++)
+                Console.WriteLine("moade [{0}] ={1} ", i, moadel[i]);
+            for (int a = 0 ; a <moadel.Length- 1 ; a++)
+                for (int j= 0 ; j <moadel.Length- 1 ; j++)
+                {
+                    if (moadel[j] > moadel[j + 1])
+                    {
+                        double temp = moadel[j];
+                        moadel[j] = moadel[j + 1];
+                        moadel[j + 1] = temp;
+                    }
+                }
+
+            for (i = 0; i < moadel.Length-1; i++)
+                Console.Write("   " + moadel[i]);
+            for (i = 0; i < moadel.Length-1; i++) 
+            {
+                if (moadel[i] > max)
+                    max = moadel[i];
+                else if (moadel[i] < min)
+                    min = moadel[i];
+            }
+            Console.ReadKey();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(" max={0}  min={1}", max, min);
+            Console.ReadKey();
+            double avg = sum / moadel.Length;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(" sum={0}   avg={1}", sum, avg);
+            Console.ReadKey();
+
+        }
+    }
+}
