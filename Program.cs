fc6497b94a981d32b836c7efeb61f9371666c62c
Zeinab-khalili86 <zinatkhalili3@gmail.com>
2025-11-20 22:52:45 +0330

    Add files via upload
    The program above is for calculating the tax percentage, the insurance percentage, and deducting them from the received salary. These programs help train our logic so that in other situations—not only salary calculation—we can use coding to subtract or add certain percentages to an initial value and display a final amount as the output

--- Program.cs
+++ Program.cs
@@ -4,31 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1_9
+namespace _1_2
 {
     class Program
     {
-        private static int masahat(int t1, int a1)
-        {
-            int mas = t1 * a1;
-            return mas;
-        }
-        private static int mohit(int t1, int a1)
-        {
-            int moh = (t1 + a1)*2;
-            return moh;
-        }
         static void Main(string[] args)
         {
-            for (int i = 1; i < 6; i++)
+            double nome = 0, number1 = 0, number2 = 0, number3 = 0, number4 = 0, number5 = 0;
+            for (int i = 1; i <= 10; i += 1)
             {
-                Console.Write(" tool[{0}] :", i);
-                int tool = int.Parse(Console.ReadLine());
-                Console.Write(" arz[{0}] :", i);
-                int arz = int.Parse(Console.ReadLine());
-                Console.WriteLine(" masahat={0}    mohit={1}  ", masahat(tool, arz), mohit(tool, arz));
+                Console.Write(" nomre nafare " + i + ":");
+                nome = double.Parse(Console.ReadLine());
+                if (nome >= 0 && nome < 11)
+                {
+                    number1 += 1;
+                }
+                else if (nome > 10 && nome < 15)
+                {
+                    number2 += 1;
+                }
+                else if (nome > 14 && nome < 18)
+                {
+                    number3 += 1;
+                }
+                else if (nome > 17 && nome < 20)
+                {
+                    number4 += 1;
+                }
+                else if (nome == 20)
+                {
+                    number5 += 1;
+                }
             }
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Console.ForegroundColor =ConsoleColor. DarkRed;
+            Console.WriteLine("nomarat bein 0-10 = " + number1);
+            Console.WriteLine("nomarat bein 11-14 = " + number2);
+            Console.WriteLine("nomarat bein 15-17 = " + number3);
+            Console.WriteLine("nomarat bein 18-19 = " + number4);
+            Console.WriteLine("nomarat  20 = " + number5);
             Console.ReadKey();
+                  
         }
     }
 }
