a33c7dbe42bd4941522b07cabb6b04cf6246b4fe
Zeinab-khalili86 <zinatkhalili3@gmail.com>
2025-11-20 22:56:16 +0330

    Add files via upload
    The program above is for calculating the tax percentage, the insurance percentage, and deducting them from the received salary. These programs help train our logic so that in other situations—not only salary calculation—we can use coding to subtract or add certain percentages to an initial value and display a final amount as the output

---
+++ 1-4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int i = int.Parse(Console.ReadLine()),sum=0, i2=i;
+            while (i > 0)
+            {
+                int a = i % 10;
+                sum +=a;
+                i = i / 10;
+            }
+            if (sum % 3 == 0)
+                Console.WriteLine(" majmoo ragham haye adad " + i2 + " bar 3 bakhsh pazir ast");
+            else
+                Console.WriteLine(" majmoo ragham haye adad " + i2 + " bar 3 bakhsh pazir nist");
+            Console.ReadKey();*/
+            int i = int.Parse(Console.ReadLine()), sum = 0, i2 = i;
+            do
+            {
+                int a = i % 10;
+                sum += a;
+                i = i / 10;
+            }
+            while (i > 0);
+            if (sum % 3 == 0)
+                Console.WriteLine(" majmoo ragham haye adad " + i2 + " bar 3 bakhsh pazir ast");
+            else
+                Console.WriteLine(" majmoo ragham haye adad " + i2 + " bar 3 bakhsh pazir nist");
+            Console.ReadKey();
+
+        }
+    }
+}
