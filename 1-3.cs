b73f0b583fc74ccdd7d9c45836508e2089e53b9d
Zeinab-khalili86 <zinatkhalili3@gmail.com>
2025-11-20 22:55:35 +0330

    Add files via upload
    The program above is for calculating the tax percentage, the insurance percentage, and deducting them from the received salary. These programs help train our logic so that in other situations—not only salary calculation—we can use coding to subtract or add certain percentages to an initial value and display a final amount as the output

---
+++ 1-3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >= 1; i -= 5)
+                Console.Write(  i+"    ");
+            Console.ReadKey();
+        }
+    }
+}
