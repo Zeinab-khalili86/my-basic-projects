2fe5e7fb2936dd94fb5a3f1e083d658e13433eda
Zeinab-khalili86 <zinatkhalili3@gmail.com>
2025-11-20 22:58:08 +0330

    Add files via upload
    The program above is for calculating the tax percentage, the insurance percentage, and deducting them from the received salary. These programs help train our logic so that in other situations—not only salary calculation—we can use coding to subtract or add certain percentages to an initial value and display a final amount as the output

---
+++ 1-8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_9
+{
+    class Program
+    {
+        private static int masahat(int t1, int a1)
+        {
+            int mas = t1 * a1;
+            return mas;
+        }
+        private static int mohit(int t1, int a1)
+        {
+            int moh = (t1 + a1)*2;
+            return moh;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write(" tool[{0}] :", i);
+                int tool = int.Parse(Console.ReadLine());
+                Console.Write(" arz[{0}] :", i);
+                int arz = int.Parse(Console.ReadLine());
+                Console.WriteLine(" masahat={0}    mohit={1}  ", masahat(tool, arz), mohit(tool, arz));
+            }
+            Console.ReadKey();
+        }
+    }
+}
